393c9f0387e3da0b1dc82ad858658e89b1c2c29a
sarthaksavvy <bitfumes@gmail.com>
2021-04-20 12:20:50 +0530

    06 Django - using templates

--- books/views.py
+++ books/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello Books App')
+    context = {'name': 'Sarthak'}
+    return render(request, 'books/index.html', context)
