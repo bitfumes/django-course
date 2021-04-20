d925138466198278d300729b5f9800f07e931137
sarthaksavvy <bitfumes@gmail.com>
2021-04-20 13:46:56 +0530

    10 Django - template filter and tags

--- books/urls.py
+++ books/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:id>', views.show)
+    path('', views.index, name='book.all'),
+    path('<int:id>', views.show, name="book.show")
 ]
