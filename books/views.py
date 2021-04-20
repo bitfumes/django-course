7e1465ff7b2ad148af02af6a37ef4fa91972fb82
sarthaksavvy <bitfumes@gmail.com>
2021-04-20 12:41:11 +0530

    07 Django - load data from json file

--- books/views.py
+++ books/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import json
+
+booksData = open(
+    '/Users/sarthak/Documents/Bitfumes/Django Course/bookstore-project/books.json').read()
+
+data = json.loads(booksData)
 
 
 def index(request):
-    context = {'name': 'Sarthak'}
+    context = {'books': data}
     return render(request, 'books/index.html', context)
