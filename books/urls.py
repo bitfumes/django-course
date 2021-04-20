c389bce9711227444b904682ebd30896a974c4de
sarthaksavvy <bitfumes@gmail.com>
2021-04-20 13:24:07 +0530

    09 Django - dynamic routing

--- books/urls.py
+++ books/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('<int:id>', views.show)
 ]
--- books/views.py
+++ books/views.py
@@ -10,3 +10,13 @@ data = json.loads(booksData)
 def index(request):
     context = {'books': data}
     return render(request, 'books/index.html', context)
+
+
+def show(request, id):
+    singleBook = list()
+    for book in data:
+        if book['id'] == id:
+            singleBook = book
+
+    context = {'book': singleBook}
+    return render(request, 'books/show.html', context)
