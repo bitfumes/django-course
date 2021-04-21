a7199d36ecd69f5ed20410a2b4eb573ab496f1fa
sarthaksavvy <bitfumes@gmail.com>
2021-04-21 13:11:36 +0530

    12 Django - migrations update

--- books/models.py
+++ books/models.py
@@ -5,3 +5,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=256)
+    pageCount = models.IntegerField(default=0)
+    thumbnailUrl = models.CharField(max_length=256, null=True)
+    shortDescription = models.CharField(max_length=256, null=True)
+    longDescription = models.TextField(null=True)
